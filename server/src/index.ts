021b24a04768b7a29ae0064e8bee808bcceed768
Anmol498 <anmoldeepsingh3886@gmail.com>
2026-02-24 20:37:50 +0530

    Allow PATCH HTTP method in CORS configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -169,7 +169,7 @@ app.use(cors({
     "http://localhost:5173"
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.set("trust proxy", 1);
